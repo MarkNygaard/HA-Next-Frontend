1d875999dc5dd227bcd18fdfb0eefce8cb44ee51
Mark Nygaard <marknygaard@hotmail.com>
2022-03-05 21:59:36 +0100

    Details Popup in progress

--- components/rooms/bathroom.tsx
+++ components/rooms/bathroom.tsx
@@ -5,32 +5,12 @@ import { useState, useEffect } from 'react';
 import DetailsWindow from '@components/details/details';
 
 const Bathroom = () => {
-  const entity_id = 'light.living_room';
-  const temp_id = 'sensor.living_room_temperature';
-  const humid_id = 'sensor.living_room_humidity';
-
-  const allEntities = [
-    { entity_name: 'Dining Table', entity_id: 'light.dining_table' },
-    { entity_name: 'Lightstrip', entity_id: 'light.lightstrip' },
-    { entity_name: 'Floor Lamp', entity_id: 'light.living_room_floor_lamp' },
-    { entity_name: 'Cornor Lamp', entity_id: 'light.corner_lamp' },
-  ];
-  const allEntityNames = [
-    { entity_name: 'Dining Table' },
-    { entity_name: 'Lightstrip' },
-    { entity_name: 'Floor Lamp' },
-    { entity_name: 'Cornor Lamp' },
-  ];
-  const allEntityIds = [
-    { entity_id: 'light.dining_table' },
-    { entity_id: 'light.lightstrip' },
-    { entity_id: 'light.living_room_floor_lamp' },
-    { entity_id: 'light.corner_lamp' },
-  ];
+  const entity_id = 'light.bathroom';
+  const temp_id = 'sensor.bathroom_temperature';
+  const humid_id = 'sensor.bathroom_humidity';
 
   const { connection } = useHassStore();
   const entity = useEntity(entity_id);
-
   const temp = useEntity(temp_id);
   const humid = useEntity(humid_id);
 
@@ -88,13 +68,13 @@ const Bathroom = () => {
           <div className="flex justify-center px-12">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 640 512"
+              viewBox="0 0 512 512"
               className="h-16 w-16 text-tile-svg dark:text-black"
               fill="currentColor"
               stroke="currentColor"
               strokeWidth="2"
             >
-              <path d="M592 224C565.5 224 544 245.5 544 272V352H96V272C96 245.5 74.51 224 48 224S0 245.5 0 272v192C0 472.8 7.164 480 16 480h64c8.836 0 15.1-7.164 15.1-16L96 448h448v16c0 8.836 7.164 16 16 16h64c8.836 0 16-7.164 16-16v-192C640 245.5 618.5 224 592 224zM128 272V320h384V272c0-38.63 27.53-70.95 64-78.38V160c0-70.69-57.31-128-128-128H191.1c-70.69 0-128 57.31-128 128L64 193.6C100.5 201.1 128 233.4 128 272z" />
+              <path d="M288 384c-17.67 0-32 14.33-32 32c0 17.67 14.33 32 32 32s32-14.33 32-32C320 398.3 305.7 384 288 384zM416 256c-17.67 0-32 14.33-32 32c0 17.67 14.33 32 32 32s32-14.33 32-32C448 270.3 433.7 256 416 256zM480 192c-17.67 0-32 14.33-32 32c0 17.67 14.33 32 32 32s32-14.33 32-32C512 206.3 497.7 192 480 192zM288 320c0-17.67-14.33-32-32-32s-32 14.33-32 32c0 17.67 14.33 32 32 32S288 337.7 288 320zM320 224c-17.67 0-32 14.33-32 32c0 17.67 14.33 32 32 32s32-14.33 32-32C352 238.3 337.7 224 320 224zM384 224c17.67 0 32-14.33 32-32c0-17.67-14.33-32-32-32s-32 14.33-32 32C352 209.7 366.3 224 384 224zM352 320c-17.67 0-32 14.33-32 32c0 17.67 14.33 32 32 32s32-14.33 32-32C384 334.3 369.7 320 352 320zM347.3 91.31l-11.31-11.31c-6.248-6.248-16.38-6.248-22.63 0l-6.631 6.631c-35.15-26.29-81.81-29.16-119.6-8.779L170.5 61.25C132.2 22.95 63.65 18.33 21.98 71.16C7.027 90.11 0 114.3 0 138.4V464C0 472.8 7.164 480 16 480h32C56.84 480 64 472.8 64 464V131.9c0-19.78 16.09-35.87 35.88-35.87c9.438 0 18.69 3.828 25.38 10.5l16.61 16.61C121.5 160.9 124.3 207.6 150.6 242.7L144 249.4c-6.248 6.248-6.248 16.38 0 22.63l11.31 11.31c6.248 6.25 16.38 6.25 22.63 0l169.4-169.4C353.6 107.7 353.6 97.56 347.3 91.31z" />
             </svg>
           </div>
         </button>
@@ -103,20 +83,19 @@ const Bathroom = () => {
           onClick={() => setIsOpen(true)}
         >
           <div className="flex w-full justify-center pt-3 text-xl font-normal text-white dark:text-black">
-            Living Room
+            Bathroom
           </div>
           <div className="flex w-full justify-center text-xs font-thin text-white">
             {temp?.state} °C / {humid?.state}%
           </div>
         </button>
         <DetailsWindow
-          entity_id={'light.dining_table'}
-          entity_name={'Dining Table'}
-          allEntities={allEntities}
+          entity_id={'light.bathroom'}
+          entity_name={'Bathroom Light'}
           open={isOpen}
           onClose={() => setIsOpen(false)}
         >
-          CRAZY DETAILS Living Room!
+          CRAZY DETAILS!
         </DetailsWindow>
       </div>
     </div>
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -4,8 +4,8 @@ import Hallway from '@components/rooms/hallway';
 import Kitchen from '@components/rooms/kitchen';
 import LivingRoom from '@components/rooms/living-room';
 import Bedroom from '@components/rooms/bedroom';
-import WalkInCloset from '../components/rooms/walk-in-closet';
-import Bathroom from '../components/rooms/bathroom';
+import WalkInCloset from '@components/rooms/walk-in-closet';
+import Bathroom from '@components/rooms/bathroom';
 import Layout from '@components/layout';
 import AllLights from '@components/rooms/all-lights';
 
